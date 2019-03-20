19b49d5ed910bdf84bac8173be62079b9a8ffa5c
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-03-19 20:27:15 -0700

    Fecha actual

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var nombre, desc, imagen;
 var i = 1; //Index del numero de post
+var today;
 
 function post() {
     nombre = document.getElementById("etxtUsu").value;
     desc = document.getElementById("etxtDesc").value;
     imagen = document.getElementById("etxtImg").value;
+    var date = new Date();
+    today = date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
     createDiv();
 }
 
@@ -12,5 +15,5 @@ function createDiv() {
     document.body.innerHTML += "<div id='card"+ (i++) +"' class='post'><div class='izquierda'><img src='" + imagen + 
     "'></div><div class='centro'><b>Usuario: </b>" + nombre + 
     "<br><br><b>Descripción: </b>" + desc + 
-    "<br><br><b>Fecha: </b> 19 de marzo de 2019 <br><br></div><div class='derecha'><button>Comentar</button></div></div><hr>";
+    "<br><br><b>Fecha: </b>"+ today +"<br><br></div><div class='derecha'><button>Comentar</button></div></div><hr>";
 }
